1d153405e94125cd8b9e0126a5f4dba20a2dca07
VedadDe <vedad.delic.student@gmail.com>
2024-06-18 15:18:47 +0200

    feat: update comments and added two more api calls

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from pydantic import BaseModel
 from tensorflow.keras.models import load_model
 import numpy as np
 from sentence_transformers import SentenceTransformer
+import logging
+from fuzzywuzzy import fuzz
+
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
@@ -10,24 +14,48 @@ app = FastAPI()
 model = load_model('C:/Users/delic/Desktop/taslEvMl/taskEvolt/application_back/model.h5')
 embedding_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-
-@app.post('/predict')
+#poziva ann trenirani model i na osnovu njega donosi odluku
+@app.post('/predict/ann')
 async def predict(productOne: str, productTwo: str):
     try:
         #print(productOne, productTwo)
         productOneEmbeddings = embedding_model.encode(productOne)
         productTwoEmbeddings = embedding_model.encode(productTwo)
         
-        #bez ovog javlja error dimenzija Exception encountered when calling Sequential.call().\n\n\u001b[1mInvalid input shape for
-        #input Tensor("data:0", shape=(32,), dtype=float32). Expected shape (None, 768), but input has incompatible shape (32,)
-
-        productOneEmbeddings = np.expand_dims(productOneEmbeddings, axis=0)
-        productTwoEmbeddings = np.expand_dims(productTwoEmbeddings, axis=0)
-        
-        
-        productPairEmbeddings = np.concatenate((productOneEmbeddings, productTwoEmbeddings), axis=1)
+        # Najproblematičnija linija tokom pisanja koda, obzirom da rad sa spajanjem embedinga je očekivao liste a obični stringovi su u početku proslijeđeni       
+        productPairEmbeddings = np.concatenate(([productOneEmbeddings], [productTwoEmbeddings]), axis=1)
         predictions = model.predict(productPairEmbeddings)
-        return {'prediction': predictions}
+        #predikcije su vraćale izvorno matricu. ista se nije mogla direktno vratiti. potrebno je izvući tačnu vrijednost i potom je striktno u float pretvorit. 
+        return {'prediction': float(predictions[0][0])}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+# zanimljiv poziv radi komparacije, koristenje samo uvezenih embeddinga koji potom nisu trenirani nad datasetom. vraća sličnost na osnovu uvezenih emdeddinga između riječi
+@app.post('/predict/just/embedding')
+async def predict(productOne: str, productTwo: str):
+    try:
+        productOneEmbeddings = embedding_model.encode(productOne)
+        productTwoEmbeddings = embedding_model.encode(productTwo)
+        
+        similarities = embedding_model.similarity(productOneEmbeddings, productTwoEmbeddings)
+        # povratna vrijednsot sličnosti je bila problematićna, obzirom da je vraćala tenzor. Stoga najviše vremena u ovoj funkciji je utrošeno na način pretvaranja tenzora. 
+        logging.info("Similarity:", similarities)
+        
+        return {'Similarity': similarities.item()}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+# biran je token set ratio na osnovu njegove definicije iz priloga koja je preuzeta sa: https://www.analyticsvidhya.com/blog/2021/07/fuzzy-string-matching-a-hands-on-guide/
+# Token set ratio performs a set operation that takes out the common tokens instead of just tokenizing the strings, 
+# sorting, and then pasting the tokens back together. Extra or same repeated words do not matter.
+# Dakle bolja poklapanja za naš slučaj se mogu izabrati ovom funkcijom u odnosu na ostale (primjer kad se gledaju sličnosti riječi jedna za drugom); 
+
+@app.post('/predict/fuzzywuzzy')
+async def predict(productOne: str, productTwo: str):
+    try:
+        result = fuzz.token_set_ratio(productOne, productTwo)
+        
+        return {'similarity': result}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
